93a7d97653cc85bc21e0e263cd745171be001f67
Everyday465 <110691604+Everyday465@users.noreply.github.com>
2025-04-14 17:04:16 +0800

    clipboard & cursor

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { Box, Container } from '@mui/material';
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
 import React from 'react';
 import "./homepage.css";
 import { downloadData } from 'aws-amplify/storage';
@@ -10,6 +11,28 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const HomePage: React.FC = () => {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = async () => {
+    try {
+      await navigator.clipboard.writeText("agnoteelijah@gmail.com");
+      setOpen(true);
+    } catch (err) {
+      console.error("Failed to copy email: ", err);
+    }
+  };
+
+  const handleClose = (
+    _event: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const downloadResume = async () => {
     try {
@@ -59,7 +82,7 @@ const HomePage: React.FC = () => {
           <GitHubIcon className="icon" />
         </a>
 
-        <a className="slide-in" href="mailto:agnoteelijah@gmail.com" title="Email">
+        <a className="slide-in" onClick={handleClick} title="Email">
           <EmailIcon className="icon" />
         </a>
 
@@ -67,6 +90,12 @@ const HomePage: React.FC = () => {
           <DescriptionIcon className="icon" />
         </a>
       </Container>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message="agnoteelijah@gmail.com has been copied to clipboard!"
+      />
     </Box>
   );
 };
